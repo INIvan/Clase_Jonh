f6db4faa061193f6858d6a3232fc26c5af9df474
IN_Ivan <43867369+INIvan@users.noreply.github.com>
2019-03-26 18:41:17 -0600

    Update config.php
    Agregación de información a las 4 leyes del vestuario de Oskar Schlemmer.

--- Proyecto_Triadico/app/config.php
+++ Proyecto_Triadico/app/config.php
@@ -1,62 +1,43 @@
 <?php
 define('SITE', array(
   'name' => 'Ballet Triádico - Oskar Schelemmer',
-  'logo' => '',
+  'logo' => 'Las formas cúbicas son enviadas al cuerpo
+  humano. Son cubos simples. Estos prismas son sustituidos 
+  por las partes del actor. Tanto la cabeza, torso, brazos etc.
+  El danzante se transforma en una "Arquitectura andante"',
   'service_cards' => array(
     array(
       'title' => 'Ley espacio cúbico',
-      'text' => 'Lorem ipsum dolor sit amet, 
-      consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit 
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-      occaecat cupidatat non proident, sunt in culpa qui officia 
-      deserunt mollit anim id est laborum.',
+      'text' => '',
       'img' => 'img/ley_cubo.jpg',
     ),
 
     array(
       'title' => 'Ley espacio - cuerpo',
-      'text' => 'Lorem ipsum dolor sit amet, 
-      consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit 
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-      occaecat cupidatat non proident, sunt in culpa qui officia 
-      deserunt mollit anim id est laborum.',
+      'text' => 'En este apartado ya no se disminuye en una
+      sola representación geométrica, si no, en una tripificación hacia el cuerpo humano.
+      En cada se relaciona en su semejanza natural del mismo cuerpo humano.
+      Con las forma ovalada, la forma esférica entre otras más. Como resutado da
+      "La marioneta"',
       'img' => 'img/ley_espacio.jpg',
  
     ),
 
     array(
-      'title' => 'Ley del movimiento',
-      'text' => 'Lorem ipsum dolor sit amet, 
-      consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit 
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-      occaecat cupidatat non proident, sunt in culpa qui officia 
-      deserunt mollit anim id est laborum.',
+      'title' => 'Ley del movimiento humano en el espacio',
+      'text' => 'Las formas se darán con el movimiento junto con al danzante.
+Movimiento con características de rotación como por ejemplo: En espiral, disco, caracol.
+El actor se convierte en un "Organismo Ténico".',
       'img' => 'img/ley_movimiento.jpg',
      
     ),
     array(
       'title' => 'Ley de la expresión metafísica',
-      'text' => 'Lorem ipsum dolor sit amet, 
-      consectetur adipiscing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit 
-      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-      occaecat cupidatat non proident, sunt in culpa qui officia 
-      deserunt mollit anim id est laborum.',
+      'text' => 'Por última ley, se da la simbolización de las partes del cuerpo humano.
+      Se utilizan las formas irreales en relación con las partes del cuerpo humano. Formas como:
+      La estrella en representación con la mano, la cruz sobre la columna. Schlemmer  
+ da como resultado la "Desmaterialización". Cerrando con el desplazamiento entre el espacio
+ y tiempo. La misticidad',
       'img' => 'img/ley_exp.jpg',
      
     )
